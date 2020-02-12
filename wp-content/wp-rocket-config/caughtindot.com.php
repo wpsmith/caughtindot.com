df4671494986b8a914310d96354996326d3bb4e4
Travis Smith <t@wpsmith.net>
2020-02-12 18:03:06 -0500

    wp-rocket

---
+++ wp-content/wp-rocket-config/caughtindot.com.php
@@ -0,0 +1,36 @@
+<?php
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+
+$rocket_cookie_hash = '64f5de0e2d0ae78bc86594839f4200b1';
+$rocket_logged_in_cookie = 'wordpress_logged_in_64f5de0e2d0ae78bc86594839f4200b1';
+$rocket_cache_mobile_files_tablet = 'desktop';
+$rocket_secret_cache_key = '5c756f54e1dfb270667878';
+$rocket_cache_mobile = 1;
+$rocket_do_caching_mobile_files = 0;
+$rocket_cache_ssl = 1;
+$rocket_cache_reject_uri = '/(.+/)?feed/?|/(?:.+/)?embed/|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_.+|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array(
+  0 => 'q',
+);
+$rocket_cache_ignored_parameters = array(
+  'utm_source' => 1,
+  'utm_medium' => 1,
+  'utm_campaign' => 1,
+  'utm_expid' => 1,
+  'utm_term' => 1,
+  'utm_content' => 1,
+  'fb_action_ids' => 1,
+  'fb_action_types' => 1,
+  'fb_source' => 1,
+  'fbclid' => 1,
+  'gclid' => 1,
+  'age-verified' => 1,
+  'ao_noptimize' => 1,
+  'usqp' => 1,
+  'cn-reloaded' => 1,
+  '_ga' => 1,
+);
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array();
---
+++ wp-content/wp-rocket-config/cid-staging.q9hwj0b7-liquidwebsites.com.php
@@ -0,0 +1,18 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+$rocket_cookie_hash = '86fb2b617c1b4f1d3200f21191740d81';
+$rocket_cache_mobile_files_tablet = 'desktop';
+$rocket_secret_cache_key = '5c756f54e1dfb270667878';
+$rocket_cache_mobile = '1';
+$rocket_do_caching_mobile_files = '0';
+$rocket_cache_ssl = '1';
+$rocket_cache_reject_uri = '/(.+/)?feed/?|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array (
+  0 => 'q',
+);
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array (
+);
